6cf8b2fa9a2a54f787d84e5b645edd7853df15bd
Robin Onsay <hello@robinonsay.com>
2025-04-12 04:22:12 -0500

    :sparkles: Add map headers

---
+++ inc/juno/map/map_api.h
@@ -0,0 +1,17 @@
+#ifndef JUNO_MAP_API_H
+#define JUNO_MAP_API_H
+#include "juno/status.h"
+#include <stddef.h>
+#include <stdint.h>
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "juno/map/map_types.h"
+
+#ifdef __cplusplus
+}
+#endif
+#endif
+
+
