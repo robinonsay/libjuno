bebd2e9c7e75463dbf35f1f129c3667019a22943
robinonsay <robinonsay@users.noreply.github.com>
2025-04-26 23:51:09 +0000

    deploy: f792a03eee61de0804b4fa4958b8416c09cb5cf7

--- search/all_7.js
+++ search/all_7.js
@@ -1,7 +1,8 @@
 var searchData=
 [
-  ['concat_0',['Concat',['../structJUNO__STRING__API__TAG.html#a1f277f8a812af2f222b269c0d02d17c3',1,'JUNO_STRING_API_TAG']]],
-  ['current_20modules_1',['Current Modules',['../index.html#autotoc_md25',1,'']]],
-  ['custom_20allocator_2',['How to Integrate Your Custom Allocator',['../md__2home_2runner_2work_2libjuno_2libjuno_2include_2juno_2memory_2README.html#autotoc_md16',1,'']]],
-  ['custom_20allocators_3',['Custom Allocators',['../md__2home_2runner_2work_2libjuno_2libjuno_2include_2juno_2memory_2README.html#autotoc_md15',1,'']]]
+  ['com_20robinonsay_20libjuno_20libjuno_20a_0',['&lt;a href=&quot;https://github.com/robinonsay/libjuno&quot; &gt;LibJuno&lt;/a&gt;',['../index.html#autotoc_md20',1,'']]],
+  ['concat_1',['Concat',['../structJUNO__STRING__API__TAG.html#a1f277f8a812af2f222b269c0d02d17c3',1,'JUNO_STRING_API_TAG']]],
+  ['current_20modules_2',['Current Modules',['../index.html#autotoc_md25',1,'']]],
+  ['custom_20allocator_3',['How to Integrate Your Custom Allocator',['../md__2home_2runner_2work_2libjuno_2libjuno_2include_2juno_2memory_2README.html#autotoc_md16',1,'']]],
+  ['custom_20allocators_4',['Custom Allocators',['../md__2home_2runner_2work_2libjuno_2libjuno_2include_2juno_2memory_2README.html#autotoc_md15',1,'']]]
 ];
--- search/all_f.js
+++ search/all_f.js
@@ -1,6 +1,8 @@
 var searchData=
 [
   ['length_0',['Length',['../structJUNO__STRING__API__TAG.html#ae962e0293bfa4d8efb9ec9f9ecf19f0d',1,'JUNO_STRING_API_TAG']]],
-  ['libjuno_1',['libjuno',['../index.html',1,'LibJuno'],['../index.html#autotoc_md20',1,'LibJuno'],['../index.html#autotoc_md21',1,'Using LibJuno']]],
-  ['load_2',['Load',['../structJUNO__TABLE__API__TAG.html#a6eb505179e22b977bbaed3d6cfffc273',1,'JUNO_TABLE_API_TAG']]]
+  ['libjuno_1',['libjuno',['../index.html',1,'LibJuno'],['../index.html#autotoc_md21',1,'Using LibJuno']]],
+  ['libjuno_20a_2',['&lt;a href=&quot;https://github.com/robinonsay/libjuno&quot; &gt;LibJuno&lt;/a&gt;',['../index.html#autotoc_md20',1,'']]],
+  ['libjuno_20libjuno_20a_3',['&lt;a href=&quot;https://github.com/robinonsay/libjuno&quot; &gt;LibJuno&lt;/a&gt;',['../index.html#autotoc_md20',1,'']]],
+  ['load_4',['Load',['../structJUNO__TABLE__API__TAG.html#a6eb505179e22b977bbaed3d6cfffc273',1,'JUNO_TABLE_API_TAG']]]
 ];
