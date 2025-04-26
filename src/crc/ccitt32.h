34b68537281d061d6748c7f40b0af4c19df4738e
Robin Onsay <hello@robinonsay.com>
2025-04-26 07:55:20 -0500

    :sparkles: Change crc lookup to const
    Ref #10

--- src/crc/ccitt32.h
+++ src/crc/ccitt32.h
@@ -1,5 +1,5 @@
 #include <stdint.h>
-static uint32_t ccitt32_crctab[256] = {
+static const uint32_t ccitt32_crctab[256] = {
     0x00000000, 0x04c11db7, 0x09823b6e, 0x0d4326d9,
     0x130476dc, 0x17c56b6b, 0x1a864db2, 0x1e475005,
     0x2608edb8, 0x22c9f00f, 0x2f8ad6d6, 0x2b4bcb61,
