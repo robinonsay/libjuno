7494d072a1faf60448c68c6c74217149db08fc81
robinonsay <robinonsay@users.noreply.github.com>
2025-06-09 17:16:21 +0000

    deploy: 6eff3c5f5f69ebf4027358e777407679fe8c8725

--- async__io__api_8h.js
+++ async__io__api_8h.js
@@ -2,7 +2,7 @@ var async__io__api_8h =
 [
     [ "JUNO_ASYNC_IO_API_TAG", "structJUNO__ASYNC__IO__API__TAG.html", "structJUNO__ASYNC__IO__API__TAG" ],
     [ "JUNO_ASYNC_IO_API_T", "async__io__api_8h.html#ad70d585b8fa8ea7a955c149ebd8e31f3", null ],
-    [ "JUNO_MODULE_BASE", "async__io__api_8h.html#aa719522822ce4b574b886d3d1242399a", null ],
+    [ "JUNO_MODULE_BASE", "async__io__api_8h.html#a1125cc6efa6f310686f1d0a1e496c249", null ],
     [ "JUNO_MODULE_BASE_DECLARE", "async__io__api_8h.html#a1a4ffea39ed94a17235b63c2d7ea496f", null ],
     [ "JUNO_MODULE_DECLARE", "async__io__api_8h.html#a4e983502fe0ba03fd3855f7f4a6e1cf0", null ]
 ];
