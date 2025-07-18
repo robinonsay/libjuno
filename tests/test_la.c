225467d61b653e6c860f968477d99cc409a7b62b
Robin Onsay <hello@robinonsay.com>
2025-07-18 07:19:09 -0500

    :sparkles: Add vec

--- tests/test_la.c
+++ tests/test_la.c
@@ -13,7 +13,8 @@ void tearDown(void)
 {
 
 }
-JUNO_MATH_MAT_NxM(MAT_3x3, 3, 3);
+JUNO_MATH_MAT_NxM_FLOAT(MAT_3x3, 3, 3);
+JUNO_MATH_VEC_N_FLOAT(VEC_3_T, 3);
 
 static void test_mat_index(void)
 {
@@ -24,9 +25,21 @@ static void test_mat_index(void)
 		for(size_t j = 0; j < 3; j++)
 		{
 			TEST_ASSERT_EQUAL_FLOAT(tTruth[i][j], tMat3x3.tMat[i][j]);
-			JUNO_MATH_MAT_NxM_T *ptMatNxM = (JUNO_MATH_MAT_NxM_T*) &tMat3x3;
+			JUNO_MATH_MAT_NxM_FLOAT_T *ptMatNxM = (JUNO_MATH_MAT_NxM_FLOAT_T*) &tMat3x3;
 			TEST_ASSERT_EQUAL_FLOAT(tTruth[i][j], ptMatNxM->tMat[i*ptMatNxM->tRoot.iMDim + j]);
 		}
+	}
+}
+
+static void test_vec_index(void)
+{
+	VEC_3_T tVec = {.tRoot = {}, {0, 1, 2}};
+	JUNO_MATH_VEC_FLOAT_T *ptFVec = (JUNO_MATH_VEC_FLOAT_T *) &tVec;
+	float tTruth[3] = {0, 1, 2};
+	for(size_t j = 0; j < 3; j++)
+	{
+		TEST_ASSERT_EQUAL_FLOAT(tTruth[j], tVec.tVec[j]);
+		TEST_ASSERT_EQUAL_FLOAT(tTruth[j], ptFVec->tVec[j]);
 	}
 }
 
@@ -34,5 +47,6 @@ int main(void)
 {
 	UNITY_BEGIN();
 	RUN_TEST(test_mat_index);
+	RUN_TEST(test_vec_index);
 	return UNITY_END();
 }
