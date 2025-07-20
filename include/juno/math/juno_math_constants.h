94ac8bc950f6efe262e3f13758e38c76ec79c7c4
Robin Onsay <hello@robinonsay.com>
2025-07-20 17:11:42 -0500

    :sparkles: Add math constants

--- include/juno/math/juno_math_constants.h
+++ include/juno/math/juno_math_constants.h
@@ -7,6 +7,10 @@
 extern "C" {
 #endif
 
+#define JUNO_MATH_PI        3.141592653589793
+#define JUNO_MATH_HALF_PI   1.5707963267948966
+#define JUNO_MATH_QUART_PI  0.7853981633974483
+#define JUNO_MATH_E         2.718281828459045
 
 #ifdef __cplusplus
 }
