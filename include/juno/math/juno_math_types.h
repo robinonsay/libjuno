2e4c588dc3bea6b0e0e56db12d19a52de12b32ae
Robin Onsay <hello@robinonsay.com>
2025-07-20 17:04:14 -0500

    :sparkles: Rename math variables

--- include/juno/math/juno_math_types.h
+++ include/juno/math/juno_math_types.h
@@ -21,74 +21,74 @@ typedef union JUNO_MATH_VEC2F_TAG
 {
     struct{JUNO_FLOAT_T i, j;};
     struct{JUNO_FLOAT_T x, y;};
-    JUNO_FLOAT_T v[2];
+    JUNO_FLOAT_T arr[2];
 } JUNO_MATH_VEC2F_T;
 
 typedef union JUNO_MATH_VEC2I_TAG
 {
     struct{JUNO_INT_T i, j;};
     struct{JUNO_INT_T x, y;};
-    JUNO_INT_T v[2];
+    JUNO_INT_T arr[2];
 } JUNO_MATH_VEC2I_T;
 
 typedef union JUNO_MATH_VEC3F_TAG
 {
     struct{JUNO_FLOAT_T i, j, k;};
     struct{JUNO_FLOAT_T x, y, z;};
-    JUNO_FLOAT_T v[3];
+    JUNO_FLOAT_T arr[3];
 } JUNO_MATH_VEC3F_T;
 
 typedef union JUNO_MATH_VEC3I_TAG
 {
     struct{JUNO_INT_T i, j, k;};
     struct{JUNO_INT_T x, y, z;};
-    JUNO_INT_T v[3];
+    JUNO_INT_T arr[3];
 } JUNO_MATH_VEC3I_T;
 
 typedef union JUNO_MATH_VEC4F_TAG
 {
     struct{JUNO_FLOAT_T i, j, k, l;};
     struct{JUNO_FLOAT_T x, y, z, w;};
-    JUNO_FLOAT_T v[4];
+    JUNO_FLOAT_T arr[4];
 } JUNO_MATH_VEC4F_T;
 
 typedef union JUNO_MATH_VEC4I_TAG
 {
     struct{JUNO_INT_T i, j, k, l;};
     struct{JUNO_INT_T x, y, z, w;};
-    JUNO_INT_T v[4];
+    JUNO_INT_T arr[4];
 } JUNO_MATH_VEC4I_T;
 
 typedef struct JUNO_MATH_M3X3F_TAG
 {
-    JUNO_FLOAT_T m[3][3];
+    JUNO_FLOAT_T mat[3][3];
 } JUNO_MATH_M3X3F_T;
 
 typedef struct JUNO_MATH_M4X4F_TAG
 {
-    JUNO_FLOAT_T m[4][4];
+    JUNO_FLOAT_T mat[4][4];
 } JUNO_MATH_M4X4F_T;
 
 typedef struct JUNO_MATH_M3X3I_TAG
 {
-    JUNO_INT_T m[3][3];
+    JUNO_INT_T mat[3][3];
 } JUNO_MATH_M3X3I_T;
 
 typedef struct JUNO_MATH_M4X4I_TAG
 {
-    JUNO_INT_T m[4][4];
+    JUNO_INT_T mat[4][4];
 } JUNO_MATH_M4X4I_T;
 
 typedef union JUNO_MATH_RQUATF_TAG
 {
     struct{JUNO_FLOAT_T s, i, j, k;};
-    JUNO_FLOAT_T q[4];
+    JUNO_FLOAT_T quat[4];
 } JUNO_MATH_RQUATF_T;
 
 typedef union JUNO_MATH_RQUATI_TAG
 {
     struct{JUNO_INT_T s, i, j, k;};
-    JUNO_INT_T q[4];
+    JUNO_INT_T quat[4];
 } JUNO_MATH_RQUATI_T;
 
 #ifdef __cplusplus
