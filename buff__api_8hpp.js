9016a8ca5edf2780a74f11061d22076a57974ace
robinonsay <robinonsay@users.noreply.github.com>
2025-07-30 15:14:21 +0000

    deploy: 54e99d6a9b42d54aaf50ccc3daa0c66f70ee3d7d

--- buff__api_8hpp.js
+++ buff__api_8hpp.js
@@ -1,7 +1,7 @@
 var buff__api_8hpp =
 [
-    [ "JUNO_MODULE_DERIVE", "buff__api_8hpp.html#ac94b119cedb34a3a334462af70969b40", null ],
-    [ "JUNO_MODULE_DERIVE", "buff__api_8hpp.html#a718a949f4267ec8dde5278fee2108bac", null ],
-    [ "JUNO_MODULE_DERIVE", "buff__api_8hpp.html#a250cbf68a66b4cbb6e9cba2bd586e80f", null ],
-    [ "JUNO_MODULE_DERIVE", "buff__api_8hpp.html#ab46ddb0aeba20dba6cfd351de741723d", null ]
+    [ "JUNO_MODULE_DERIVE", "buff__api_8hpp.html#a435a2c0ef34dbc82f4aa2bce96e8a82e", null ],
+    [ "JUNO_MODULE_DERIVE", "buff__api_8hpp.html#ac81fd9d29302ed85450fe7c1261e2bec", null ],
+    [ "JUNO_MODULE_DERIVE", "buff__api_8hpp.html#ae04eae70300c5836e67b3fc9dffb8711", null ],
+    [ "JUNO_MODULE_DERIVE", "buff__api_8hpp.html#ac796676e44a90edff28f480c9657c08d", null ]
 ];
