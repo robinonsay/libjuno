028b0523805d2c479a95d183a31b7ba38619f254
Robin Onsay <hello@robinonsay.com>
2025-08-27 09:50:05 -0500

    Update status.h

--- include/juno/status.h
+++ include/juno/status.h
@@ -16,29 +16,28 @@
 */
 #ifndef JUNO_STATUS_H
 #define JUNO_STATUS_H
+#include <stdint.h>
 #ifdef __cplusplus
 extern "C" {
 #endif
-typedef enum JUNO_STATUS_TAG
-{
-    JUNO_STATUS_SUCCESS             = 0,
-    JUNO_STATUS_ERR                 = 1,
-    JUNO_STATUS_NULLPTR_ERROR       = 2,
-    JUNO_STATUS_MEMALLOC_ERROR      = 3,
-    JUNO_STATUS_MEMFREE_ERROR       = 4,
-    JUNO_STATUS_INVALID_TYPE_ERROR  = 5,
-    JUNO_STATUS_INVALID_SIZE_ERROR  = 6,
-    JUNO_STATUS_TABLE_FULL_ERROR    = 7,
-    JUNO_STATUS_DNE_ERROR           = 8,
-    JUNO_STATUS_FILE_ERROR          = 9,
-    JUNO_STATUS_READ_ERROR          = 10,
-    JUNO_STATUS_WRITE_ERROR         = 11,
-    JUNO_STATUS_CRC_ERROR           = 12,
-    JUNO_STATUS_INVALID_REF_ERROR   = 13,
-    JUNO_STATUS_REF_IN_USE_ERROR    = 14,
-    JUNO_STATUS_INVALID_DATA_ERROR  = 15,
-    JUNO_STATUS_TIMEOUT_ERROR       = 16,
-} JUNO_STATUS_T;
+typedef int32_t JUNO_STATUS_T;
+#define JUNO_STATUS_SUCCESS             0
+#define JUNO_STATUS_ERR                 1
+#define JUNO_STATUS_NULLPTR_ERROR       2
+#define JUNO_STATUS_MEMALLOC_ERROR      3
+#define JUNO_STATUS_MEMFREE_ERROR       4
+#define JUNO_STATUS_INVALID_TYPE_ERROR  5
+#define JUNO_STATUS_INVALID_SIZE_ERROR  6
+#define JUNO_STATUS_TABLE_FULL_ERROR    7
+#define JUNO_STATUS_DNE_ERROR           8
+#define JUNO_STATUS_FILE_ERROR          9
+#define JUNO_STATUS_READ_ERROR          10
+#define JUNO_STATUS_WRITE_ERROR         11
+#define JUNO_STATUS_CRC_ERROR           12
+#define JUNO_STATUS_INVALID_REF_ERROR   13
+#define JUNO_STATUS_REF_IN_USE_ERROR    14
+#define JUNO_STATUS_INVALID_DATA_ERROR  15
+#define JUNO_STATUS_TIMEOUT_ERROR       16
 
 typedef void JUNO_USER_DATA_T;
 typedef void (*JUNO_FAILURE_HANDLER_T)(JUNO_STATUS_T tStatus, const char *pcCustomMessage, JUNO_USER_DATA_T *pvUserData);
