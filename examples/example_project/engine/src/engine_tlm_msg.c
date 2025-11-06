c21319c3c6b05298a755ff1a84bb5cd761540bcb
Robin Onsay <hello@robinonsay.com>
2025-11-06 06:14:56 -0600

    Add copyright notict

--- examples/example_project/engine/src/engine_tlm_msg.c
+++ examples/example_project/engine/src/engine_tlm_msg.c
@@ -1,3 +1,20 @@
+/*
+    MIT License
+
+    Copyright (c) 2025 Robin A. Onsay
+
+    Permission is hereby granted, free of charge, to any person obtaining
+    a copy of this software and associated documentation files
+    (the "Software"), to deal in the Software without restriction,
+    including without limitation the rights to use, copy, modify, merge,
+    publish, distribute, sublicense, and/or sell copies of the Software,
+    and to permit persons to whom the Software is furnished to do so,
+    subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be
+    included in all copies or substantial portions of the Software.
+*/
+
 #include "engine_app/engine_tlm_msg.h"
 #include "juno/ds/array_api.h"
 #include "juno/macros.h"
